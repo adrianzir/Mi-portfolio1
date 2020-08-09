f8d755a03ac7c39098aae525f72d2645ee3b6f5f
Adrian <adrianzamstrong@gmail.com>
2020-08-09 18:09:44 -0400

    first commit of this branch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
 })
+var formEffect = document. getElementsByTagName(input)
+function greenColor(){
+    return input.border - color(greenyellow);
+}
+if (input == true){
+    greenColor();
+}
